41d23a7b5c18fa46747fd1a7d5fd4c268bc0de90
tolikcode <tolikcode@gmail.com>
2013-08-16 20:25:28 +0300

    Heap class draft.

---
+++ Sorting/DataStructures/Heap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmsCourse1.DataStructures
+{
+    class Heap<T> where T : IComparable
+    {
+        private List<T> data = new List<T>();
+ 
+        public void Insert(T insertValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ExtractMin()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
